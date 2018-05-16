29e323c07383b6a69a7c9783fd84350c40861f8d
Jean-Charles Dessaint <jcdessaint@gmail.com>
2018-05-16 18:10:58 +0200

    add Rx Extension files

---
+++ Sources/PlayerContextDelegateProxy.swift
@@ -0,0 +1,68 @@
+//
+//  PlayerContextDelegateProxy.swift
+//  ModernAVPlayer
+//
+//  Created by Jean-Charles Dessaint on 16/05/2018.
+//
+
+import RxCocoa
+import RxSwift
+
+extension ConcretePlayerContext: HasDelegate {
+    public typealias Delegate = PlayerContextDelegate
+}
+
+public class RxPlayerContextDelegateProxy: DelegateProxy<ConcretePlayerContext, PlayerContextDelegate>,
+    DelegateProxyType,
+PlayerContextDelegate {
+    
+    // MARK: - Initialization
+    
+    public init(playerContext: ConcretePlayerContext) {
+        super.init(parentObject: playerContext, delegateProxy: RxPlayerContextDelegateProxy.self)
+    }
+    
+    public static func registerKnownImplementations() {
+        self.register { RxPlayerContextDelegateProxy(playerContext: $0) }
+    }
+    
+    // MARK: - Proxy Subjects
+    
+    internal lazy var stateSubject = PublishSubject<ModernAVPlayer.PlayerState>()
+    internal lazy var currentTimeSubject = PublishSubject<Double?>()
+    internal lazy var itemDurationSubject = PublishSubject<Double?>()
+    internal lazy var debugMessageSubject = PublishSubject<String?>()
+    internal lazy var currentItemUrlSubject = PublishSubject<URL?>()
+    
+    // MARK: - PlayerContextDelegate
+    
+    public func playerContext(_ context: ConcretePlayerContext, state: ModernAVPlayer.PlayerState) {
+        stateSubject.onNext(state)
+    }
+    
+    public func playerContext(_ context: ConcretePlayerContext, currentTime: Double?) {
+        currentTimeSubject.onNext(currentTime)
+    }
+    
+    public func playerContext(_ context: ConcretePlayerContext, itemDuration: Double?) {
+        itemDurationSubject.onNext(itemDuration)
+    }
+    
+    public func playerContext(_ context: ConcretePlayerContext, debugMessage: String?) {
+        debugMessageSubject.onNext(debugMessage)
+    }
+    
+    public func playerContext(_ context: ConcretePlayerContext, currentItemUrl: URL?) {
+        currentItemUrlSubject.onNext(currentItemUrl)
+    }
+    
+    // MARK: - Deinit
+    
+    deinit {
+        self.stateSubject.on(.completed)
+        self.currentTimeSubject.on(.completed)
+        self.itemDurationSubject.on(.completed)
+        self.debugMessageSubject.on(.completed)
+        self.currentItemUrlSubject.on(.completed)
+    }
+}

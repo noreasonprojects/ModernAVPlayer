f390ac98fb37c5dc492188b555c8ef592ec46edd
Yaroslav Lvov <y.lvov@nullgr.com>
2020-11-18 12:37:08 +0200

    chore: update Pods/

--- Pods/Target Support Files/Pods-ModernAVPlayer_Example/Pods-ModernAVPlayer_Example-frameworks.sh
+++ Pods/Target Support Files/Pods-ModernAVPlayer_Example/Pods-ModernAVPlayer_Example-frameworks.sh
@@ -197,16 +197,16 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ModernAVPlayer/ModernAVPlayer.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ModernAVPlayer-iOS/ModernAVPlayer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa-iOS/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay-iOS/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-iOS/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ModernAVPlayer/ModernAVPlayer.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ModernAVPlayer-iOS/ModernAVPlayer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa-iOS/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay-iOS/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-iOS/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
